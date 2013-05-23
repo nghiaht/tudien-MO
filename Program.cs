5584fdb4ed1007fba2dbada5b1f2e80d1cb4b396
Huỳnh Trọng Nghĩa <Nghia.HT@hotmail.com>
2013-05-23 10:23:57 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading;
 namespace ThuNghiem
 {
     class Program
@@ -14,7 +15,7 @@ namespace ThuNghiem
             {
                 x = reader.ReadToEnd();
             }
-
+            
             Dictionary<string, string[]> DICT = new Dictionary<string, string[]>();  // TU DIEN
             string[] content = x.Split(new char[] { '@' }); // NOI DUNG
 
@@ -23,16 +24,17 @@ namespace ThuNghiem
                 if (!string.IsNullOrEmpty(s))
                 {
                     int dau_xuong_hang = s.IndexOf('\n');
+                    
                     if (dau_xuong_hang > 0)
                     {
                         // Co phat am hay khong?
-                        if ((!string.IsNullOrEmpty(s[dau_xuong_hang -1].ToString())) && s[dau_xuong_hang - 1].Equals("/"))
+                        if (s[dau_xuong_hang - 1].Equals('/'))
                         {
-                            int khoang_trang = s.IndexOf(" ",0, dau_xuong_hang); // Khoang trang truoc phan phat am /... /
+                            int khoang_trang = s.IndexOf(' ', 0, dau_xuong_hang); // Khoang trang truoc phan phat am /... /
 
                             x = s.Substring(0, khoang_trang); // WORD
                             string phatam = s.Substring(khoang_trang + 1, dau_xuong_hang - khoang_trang - 1); // Phat Am
-
+                            //Console.WriteLine(phatam);
                             // Them vao tu dien
                             if (!DICT.ContainsKey(x))
                             {
@@ -61,15 +63,30 @@ namespace ThuNghiem
                             {
                                 DICT[x][1] += s.Substring(dau_xuong_hang + 1);
                             }
-                            
+
                         }
 
                     }
+                    else
+                    {
+                        DICT.Add(s, new string[] { "" });
+                    }
                 }
             }
-
+            //string tu = "";
+            //while (!tu.Equals("exit"))
+            //{
+            //    tu = Console.ReadLine();
+            //    if (DICT.ContainsKey(tu.ToLower()))
+            //    {
+            //        Console.WriteLine("****");
+            //        Console.Write(DICT[tu][0] + '\n' + DICT[tu][1]);
+            //    }
+                    
+            //}
             ///
             Console.ReadKey();
         }
     }
 }
+    	
